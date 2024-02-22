e7cf7610a07c46d8d2011ca50eab722737d75eb4
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2024-02-22 17:22:16 -0600

    test: replace trc.load() with new trc.loadRaw() helper

--- dash-policyreport/capabilities/clusterpolicyreport.e2e.test.ts
+++ dash-policyreport/capabilities/clusterpolicyreport.e2e.test.ts
@@ -4,11 +4,11 @@ import { fullCreate, untilTrue } from "helpers/src/general";
 import { moduleUp, moduleDown } from "helpers/src/pepr";
 import { secs, mins } from "helpers/src/time";
 import { clean } from "helpers/src/cluster";
+import { gone } from "helpers/src/resource";
 import { K8s, kind } from "kubernetes-fluent-client";
 import { ClusterPolicyReport } from "../types/clusterpolicyreport-v1alpha2";
 import { UDSExemptionCRD } from "../types/uds-exemption-crd-v1alpha1";
 import { Exemption } from "../types/uds-exemption-v1alpha1";
-import { gone } from "helpers/src/resource";
 
 const trc = new TestRunCfg(__filename);
 
@@ -24,12 +24,8 @@ describe("Pepr ClusterPolicyReport()", () => {
     // want the CRD to install automagically w/ the Pepr Module startup (eventually)
     let label = "load ClusterPolicyReport CRD"
     console.time(label)
-    let crds = await trc.load(`${trc.root()}/types/wgpolicyk8s.io_clusterpolicyreports.yaml`);
-    crds = crds.map( (crd) => {
-      delete crd.metadata.labels
-      return crd
-    })
-    const crds_applied = await apply(crds);
+    const crds = await trc.loadRaw(`${trc.root()}/types/wgpolicyk8s.io_clusterpolicyreports.yaml`)
+    const crds_applied = await apply(crds)
     console.timeEnd(label)
 
     label = "load UDS Exemption CRD"
@@ -39,20 +35,20 @@ describe("Pepr ClusterPolicyReport()", () => {
     );
     console.timeEnd(label)
 
-    await moduleUp();
-  }, mins(4));
+    await moduleUp()
+  }, mins(4))
 
-  beforeEach( async () => {
+  beforeEach(async () => {
     const file = `${trc.root()}/capabilities/exemption.yaml`
 
     const label = `load resources: ${file}`
     console.time(label)
     const resources = await trc.load(file)
-    const resources_applied = await apply(resources);
+    const resources_applied = await apply(resources)
     console.timeEnd(label)
   }, secs(30))
 
-  afterEach( async () => {
+  afterEach(async () => {
     const label = "clean test-labelled resources"
     console.time(label)
     await clean(trc)
@@ -63,25 +59,17 @@ describe("Pepr ClusterPolicyReport()", () => {
   afterAll(async () => {
     const label = "teardown Pepr module"
     console.time(label)
-    await moduleDown();
+    await moduleDown()
     console.timeEnd(label)
   }, mins(2));
 
-  it(
-    "Generate policy report when there is a uds exemption",
-    async () => {
-      const cpr = await K8s(ClusterPolicyReport).Get("pepr-report");
+  it("Generate policy report when there is a uds exemption", async () => {
+      const cpr = await K8s(ClusterPolicyReport).Get("pepr-report")
       expect(cpr).not.toBeFalsy();
-    },
-    secs(30),
-  );
+  }, secs(30))
 
-  it(
-    "When there are no exemptions delete the cluster policy report",
-    async () => {
+  it("When there are no exemptions delete the cluster policy report", async () => {
       await K8s(Exemption).InNamespace("pexex-policy-report").Delete("exemption")
       await untilTrue(() => gone(ClusterPolicyReport, {metadata: {name: "pepr-report"} }))
-    },
-    secs(30),
-  );
+  }, secs(30))
 });

7f0362dce893ae4a9ac22bbaad782832d9894f2a
Case Wylie <cmwylie19@defenseunicorns.com>
2024-08-12 18:54:40 -0400

    chore: redo store e2e
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- hello-pepr-store/capabilities/store.e2e.test.ts
+++ hello-pepr-store/capabilities/store.e2e.test.ts
@@ -1,194 +1,170 @@
-import {
-  beforeAll,
-  afterAll,
-  describe,
-  it,
-  expect,
-} from "@jest/globals";
+import { beforeAll, afterAll, describe, it, expect } from "@jest/globals";
 import { kind } from "kubernetes-fluent-client";
 import { TestRunCfg } from "helpers/src/TestRunCfg";
 import { fullCreate } from "helpers/src/general";
-import { secs, mins, timed, sleep } from 'helpers/src/time';
-import { moduleUp, moduleDown, untilLogged, logs } from 'helpers/src/pepr';
-import { clean } from 'helpers/src/cluster';
+import { secs, mins, timed, sleep } from "helpers/src/time";
+import { moduleUp, moduleDown, untilLogged, logs } from "helpers/src/pepr";
+import { clean } from "helpers/src/cluster";
 
 const apply = async res => {
   return await fullCreate(res, kind);
-}
+};
 
-const trc = new TestRunCfg(__filename)
+const trc = new TestRunCfg(__filename);
 
 describe("store.ts", () => {
-  beforeAll(async () => await moduleUp(), mins(2))
+  beforeAll(async () => await moduleUp(), mins(2));
 
   afterAll(async () => {
-    await moduleDown()
-    await clean(trc)
-  }, mins(5))
+    await moduleDown();
+    await clean(trc);
+  }, mins(5));
 
   describe("module default store", () => {
     describe("data injection", () => {
-      let logz
+      let logz;
 
       beforeAll(async () => {
         await timed("load+clear: default store data", async () => {
-          await untilLogged('"msg":"DONE!"', 1)
-          logz = await logs()
-        })
-      }, mins(1))
+          await untilLogged('"msg":"onReady"', 4);
+          logz = await logs();
+        });
+      }, mins(1));
 
-      it("can insert in onReady hook", async () => {
-        const values = logz
-          .filter(l => l.includes('"key":"onReady"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.value)
-        const values2 = logz
-          .filter(l => l.includes('"key2":"sso-client-http://bin"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.value2)
-        // both controller pods + watcher pod run onReady!
-        expect(values[0]).toBe("yep")
-        expect(values[1]).toBe("yep")
-        expect(values[2]).toBe("yep")
+      it(
+        "can insert in onReady hook",
+        async () => {
+          const values = logz
+            .filter(l => l.includes('"key":"https://onReady"'))
+            .map(l => JSON.parse(l))
+            .map(o => o.value);
 
-        expect(values2[0]).toBe("yep2")
-        expect(values2[1]).toBe("yep2")
-        expect(values2[2]).toBe("yep2")
+          expect(values).toEqual(["yep", "yep", "yep"]);
+        },
+        secs(10),
+      );
 
-      }, secs(10))
+      it(
+        "can clear in onReady hook",
+        async () => {
+          const values = logz
+            .filter(l => l.includes('"key":"https://onReady"'))
+            .map(l => JSON.parse(l))
+            .map(o => o.value);
 
-
-      it("can clear in onReady hook", async () => {
-        const values = logz
-          .filter(l => l.includes('"key":"onReady"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.value)
-        const values2 = logz
-          .filter(l => l.includes('"key2":"sso-client-http://bin"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.value2)
-
-        // both controller pods + watcher pod run onReady!
-        expect(values[3]).toBe(null)
-        expect(values[4]).toBe(null)
-        expect(values[5]).toBe(null)
-        expect(values2[3]).toBe(null)
-        expect(values2[4]).toBe(null)
-        expect(values2[5]).toBe(null)
-      }, secs(10))
-    })
-
+          // both controller pods + watcher pod run onReady!
+          expect(values).toStrictEqual([
+            "yep",
+            "yep",
+            "yep",
+            undefined,
+            undefined,
+            undefined,
+          ]);
+        },
+        secs(10),
+      );
+    });
 
     describe("asynchronous interaction", () => {
-      let logz
+      let logz;
 
       beforeAll(async () => {
-        const file = `${trc.root()}/capabilities/scenario.async.yaml`
+        const file = `${trc.root()}/capabilities/scenario.async.yaml`;
         await timed(`load: ${file}`, async () => {
-          const resources = await trc.load(file)
-          const applied = await apply(resources)
-          await untilLogged('"msg":"removeItem2"')
-          logz = await logs()
-        })
-      }, mins(1))
+          const resources = await trc.load(file);
+          const applied = await apply(resources);
+          await untilLogged('"msg":"removeItem"');
+          logz = await logs();
+        });
+      }, mins(1));
 
-      it("key can be written, read, and removed", async () => {
-        const messages = logz
-          .filter(l => l.includes('"key":"async"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.msg)
-        const messages2 = logz
-          .filter(l => l.includes('"key2":"async-sso-client-http://bin"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.msg)
+      it(
+        "key can be written, read, and removed",
+        async () => {
+          const messages = logz
+            .filter(l => l.includes('"key":"https://async"'))
+            .map(l => JSON.parse(l))
+            .map(o => ({ msg: o.msg, value: o.value }));
 
-
-        expect(messages[0]).toBe("setItem")
-        expect(messages[1]).toBe("getItem")
-        expect(messages[2]).toBe("removeItem")
-
-        expect(messages2[0]).toBe("setItem2")
-        expect(messages2[1]).toBe("getItem2")
-        expect(messages2[2]).toBe("removeItem2")
-      }, secs(10))
-    })
-
+          expect(messages).toStrictEqual([
+            { msg: "setItem", value: "yep" },
+            { msg: "getItem", value: "yep" },
+            { msg: "removeItem", value: undefined },
+          ]);
+        },
+        secs(10),
+      );
+    });
 
     describe("synchronous interaction", () => {
-      let logz
+      let logz;
 
       beforeAll(async () => {
-        const file = `${trc.root()}/capabilities/scenario.sync.yaml`
+        const file = `${trc.root()}/capabilities/scenario.sync.yaml`;
         await timed(`load: ${file}`, async () => {
-          const resources = await trc.load(file)
-          const applied = await apply(resources)
+          const resources = await trc.load(file);
+          const applied = await apply(resources);
 
-          await untilLogged('"msg":"removeItemAndWait2"')
-          logz = await logs()
-        })
-      }, mins(2))
+          await untilLogged('"msg":"removeItemAndWait"');
+          logz = await logs();
+        });
+      }, mins(2));
 
-      it("key can be written, read, and removed", async () => {
+      it(
+        "key can be written, read, and removed",
+        async () => {
+          const messages = logz
+            .filter(l => l.includes('"key":"https://sync"'))
+            .map(l => JSON.parse(l))
+            .map(o => ({ msg: o.msg, value: o.value }));
 
-        const messages = logz
-          .filter(l => l.includes('"key":"sync"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.msg)
-        const messages2 = logz
-          .filter(l => l.includes('"key2":"sync-sso-client-http://bin"'))
-          .map(l => JSON.parse(l))
-          .map(o => o.msg)
-        expect(messages[0]).toBe("setItemAndWait")
-        expect(messages[1]).toBe("getItem")
-        expect(messages[2]).toBe("removeItemAndWait")
-        expect(messages2[0]).toBe("setItemAndWait2")
-        expect(messages2[1]).toBe("getItem2")
-        expect(messages2[2]).toBe("removeItemAndWait2")
-      }, secs(10))
-    })
+          expect(messages).toStrictEqual([
+            { msg: "setItemAndWait", value: undefined },
+            { msg: "getItem", value: "yep" },
+            { msg: "removeItemAndWait", value: undefined },
+          ]);
+        },
+        secs(10),
+      );
+    });
 
     describe("observed interaction", () => {
-      let logz
+      let logz;
 
       beforeAll(async () => {
-        const file = `${trc.root()}/capabilities/scenario.observe.yaml`
+        const file = `${trc.root()}/capabilities/scenario.observe.yaml`;
         await timed(`load: ${file}`, async () => {
-          const resources = await trc.load(file)
-          const applied = await apply(resources)
+          const resources = await trc.load(file);
+          const applied = await apply(resources);
 
-          await untilLogged('"msg":"final-observed"')
-          logz = await logs()
-        })
-      }, mins(1))
+          await untilLogged('"msg":"observed"');
+          logz = await logs();
+        });
+      }, mins(1));
 
-      it("sees batched update", async () => {
-        const getItem = logz.filter(l => l.includes('"msg":"got correct item"'))
-        const update = logz
-          .filter(l => l.includes('"msg":"final-observed"'))
-          .map(l => JSON.parse(l))
-          .flatMap(o => o.updates)
-          .filter(o => o.hasOwnProperty("v2-observed-sso-client-http://bin"))
+      it(
+        "sees batched update",
+        async () => {
+          const update = logz
+            .filter(l => l.includes('"msg":"observed"'))
+            .map(l => JSON.parse(l))
+            .flatMap(o => o.updates)
+            .filter(o => o.hasOwnProperty("v2-https://observed"));
 
-          let filteredData = update.map(obj => {
-            let filteredObj = {};
-            Object.keys(obj).forEach(key => {
-              if (!key.startsWith('v2-async') && !key.startsWith('v2-sync')) {
-                filteredObj[key] = obj[key];
-              }
-            });
-            return filteredObj;
-          });
-          
-        // multiple setItems batch into single store update!
-        expect(filteredData).toHaveLength(1)
- 
-        expect(filteredData[0]).toEqual(
-          expect.objectContaining({ "v2-a": "1", "v2-b": "2", "v2-c": "3", "v2-observed": "yay", "v2-observed-sso-client-http://bin": "asd-123-xyz" })
-        )
-       
-        expect(getItem).toHaveLength(1)
-      }, secs(10))
-    })
-  })
-})
+          expect(update).toHaveLength(1);
 
+          expect(update[0]).toEqual(
+            expect.objectContaining({
+              "v2-a": "1",
+              "v2-b": "2",
+              "v2-c": "3",
+              "v2-https://observed": "yay",
+            }),
+          );
+        },
+        secs(10),
+      );
+    });
+  });
+});
--- hello-pepr-store/capabilities/store.ts
+++ hello-pepr-store/capabilities/store.ts
@@ -2,7 +2,7 @@ import { Capability, a, Log } from "pepr";
 import { untilTrue } from "helpers/src/general";
 
 const name = "hello-pepr-store";
-const ssoURL = "sso-client-http://bin";
+
 export const HelloPeprStore = new Capability({
   name: name,
   description: name,
@@ -14,116 +14,64 @@ const found = key => () => Promise.resolve(!!Store.getItem(key));
 const gone = key => () => Promise.resolve(!Store.getItem(key));
 
 Store.onReady(async () => {
-  const [key, val] = ["onReady", "yep"];
-  const [key2, val2] = [ssoURL, "yep2"];
+  const [key, val] = ["https://onReady", "yep"];
 
   await Store.setItemAndWait(key, val);
-  let value = Store.getItem(key);
+  const value = Store.getItem(key);
   Log.info({ key, value }, "onReady");
-
-  await Store.setItemAndWait(key2, val2);
-  let value2 = Store.getItem(key2);
-  if (value2 === val2) {
-    Log.info({ key2, value2 }, "onReady2");
-  }
 
   Store.clear();
   await untilTrue(gone(key));
-  await untilTrue(gone(key2));
-  value = Store.getItem(key);
-  value2 = Store.getItem(key2);
-  Log.info({ key, value }, "onReady");
-  if (value2 !== val2) {
-    Log.info({ key2, value2 }, "onReady2");
-  }
-
-  Log.info("DONE!")
+  Log.info({ key }, "onReady");
 });
 
 When(a.ConfigMap)
   .IsCreated()
   .WithName("async")
   .Watch(async function asyncWatch() {
-    const [key, val] = ["async", "yep"];
-    const [key2, val2] = [`async-${ssoURL}`, "yep2"];
+    const [key, val] = ["https://async", "yep"];
 
     Store.setItem(key, val);
     await untilTrue(found(key));
-    Log.info({ key, val }, "setItem");
-
-    Store.setItem(key2, val2);
-    await untilTrue(found(key2));
-    Log.info({ key2, val2 }, "setItem2");
+    Log.info({ key, value: val }, "setItem");
 
     const value = Store.getItem(key);
     Log.info({ key, value }, "getItem");
 
-    const value2 = Store.getItem(key2);
-    if (value2 === val2) {
-      Log.info({ key2, value2 }, "getItem2");
-    }
-
     Store.removeItem(key);
     await untilTrue(gone(key));
     Log.info({ key }, "removeItem");
-
-    Store.removeItem(key2);
-    await untilTrue(gone(key2));
-    const updatedValue2 = Store.getItem(key2);
-    if (updatedValue2 !== val2) {
-      Log.info({ key2 }, "removeItem2");
-    }
   });
 
 When(a.ConfigMap)
   .IsCreated()
   .WithName("sync")
   .Watch(async function syncWatch() {
-    const [key, val] = ["sync", "yep"];
-    const [key2, val2] = [`sync-${ssoURL}`, "yep2"];
+    const [key, val] = ["https://sync", "yep"];
 
     await Store.setItemAndWait(key, val);
     Log.info({ key, val }, "setItemAndWait");
-    await Store.setItemAndWait(key2, val2);
-    Log.info({ key2, val2 }, "setItemAndWait2");
 
     const value = Store.getItem(key);
     Log.info({ key, value }, "getItem");
-    const value2 = Store.getItem(key2);
-    if (value2 === val2) {
-      Log.info({ key2, value2 }, "getItem2");
-    }
 
     await Store.removeItemAndWait(key);
     Log.info({ key }, "removeItemAndWait");
-    await Store.removeItemAndWait(key2);
-    const updatedValue2 = Store.getItem(key2);
-    if (updatedValue2 !== val2) {
-      Log.info({ key2 }, "removeItemAndWait2");
-    }
   });
 
 When(a.ConfigMap)
   .IsCreated()
   .WithName("observe")
   .Watch(async function observeWatch() {
-    const key = `observed-${ssoURL}`;
     const updates = [];
     const unsubscribe = Store.subscribe(data => updates.push(data));
 
     Store.setItem("a", "1");
     Store.setItem("b", "2");
     Store.setItem("c", "3");
-    await Store.setItemAndWait("observed", "yay");
-    await Store.setItemAndWait(key, "asd-123-xyz");
-    const item = Store.getItem(key);
-    if (item == "asd-123-xyz") {
-      Log.info({ item }, "got correct item");
-    }
+    await Store.setItemAndWait("https://observed", "yay");
 
     unsubscribe();
 
-
-    Log.info({ updates }, "final-observed");
-
+    Log.info({ updates }, "observed");
   });

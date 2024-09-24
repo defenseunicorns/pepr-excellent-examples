ccb299cdaaf1fd2cc35e40a57ea418028970f84c
Kim Schaefer <kim@defenseunicorns.com>
2024-09-24 04:30:43 -0600

    added alias child logger e2e tests

--- hello-pepr-alias/capabilities/alias.e2e.test.ts
+++ hello-pepr-alias/capabilities/alias.e2e.test.ts
@@ -1,10 +1,10 @@
-import { beforeAll, afterAll, describe, it, jest, expect } from "@jest/globals";
+import { beforeAll, afterAll, describe, it, expect } from "@jest/globals";
 import { TestRunCfg } from "helpers/src/TestRunCfg";
-import { mins, secs, timed, sleep } from "helpers/src/time";
+import { mins, secs, timed } from "helpers/src/time";
 import { fullCreate } from "helpers/src/general";
-import { moduleUp, moduleDown, untilLogged, logs } from "helpers/src/pepr";
+import { moduleUp, moduleDown, logs } from "helpers/src/pepr";
 import { clean } from "helpers/src/cluster";
-import { K8s, kind, Log } from "pepr";
+import { K8s, kind } from "pepr";
 
 const trc = new TestRunCfg(__filename);
 
@@ -21,16 +21,15 @@ describe("alias.ts", () => {
     beforeAll(async () => {
       const file = `${trc.root()}/capabilities/scenario.create.yaml`;
       await timed(`load: ${file}`, async () => {
-        let [ns, cmReconcile, cmWatch] = await trc.load(file);
-        await fullCreate([ns, cmReconcile, cmWatch]);
+        let [ns, cmReconcile, cmWatch, cmValidate, cmMutate] =
+          await trc.load(file);
+        await fullCreate([ns, cmReconcile, cmWatch, cmValidate, cmMutate]);
 
         await K8s(kind[cmReconcile.kind]).Delete(cmReconcile);
         await K8s(kind[cmWatch.kind]).Delete(cmWatch);
+        await K8s(kind[cmValidate.kind]).Delete(cmValidate);
+        await K8s(kind[cmMutate.kind]).Delete(cmMutate);
 
-        await untilLogged(
-          "Removed finalizer 'pepr.dev/finalizer' from 'hello-pepr-finalize-create/",
-          2,
-        );
         logz = await logs();
       });
     }, mins(2));
@@ -78,7 +77,208 @@ describe("alias.ts", () => {
         );
 
         expect(results).toEqual(
-          expect.not.arrayContaining([expect.stringMatching('\"alias\":')]),
+          expect.not.arrayContaining([expect.stringMatching('"alias":')]),
+        );
+      },
+      secs(10),
+    );
+  });
+
+  describe("create - watch - finalize", () => {
+    let logz: string[];
+
+    beforeAll(async () => {
+      const file = `${trc.root()}/capabilities/scenario.create.yaml`;
+      await timed(`load: ${file}`, async () => {
+        let [ns, cmReconcile, cmWatch, cmValidate, cmMutate] =
+          await trc.load(file);
+        await fullCreate([ns, cmReconcile, cmWatch, cmValidate, cmMutate]);
+
+        await K8s(kind[cmReconcile.kind]).Delete(cmReconcile);
+        await K8s(kind[cmWatch.kind]).Delete(cmWatch);
+        await K8s(kind[cmValidate.kind]).Delete(cmValidate);
+        await K8s(kind[cmMutate.kind]).Delete(cmMutate);
+
+        logz = await logs();
+      });
+    }, mins(2));
+
+    it(
+      "uses provided alias",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes('"msg":"external api call (watch-create-alias):'),
+        );
+
+        expect(results).toEqual(
+          expect.arrayContaining([
+            expect.stringMatching("alias:create:watch"),
+            expect.stringMatching("alias:create:watch:finalize"),
+          ]),
+        );
+      },
+      secs(10),
+    );
+
+    it(
+      "uses default alias",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes('"msg":"external api call (watch-create-default-alias):'),
+        );
+
+        expect(results).toEqual(
+          expect.arrayContaining([expect.stringMatching("no alias provided")]),
+        );
+      },
+      secs(10),
+    );
+
+    it(
+      "does not log alias if alias child logger not used",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes(
+            '"msg":"external api call (watch-create-no-child-logger):',
+          ),
+        );
+
+        expect(results).toEqual(
+          expect.not.arrayContaining([expect.stringMatching('"alias":')]),
+        );
+      },
+      secs(10),
+    );
+  });
+
+  describe("create - validate", () => {
+    let logz: string[];
+
+    beforeAll(async () => {
+      const file = `${trc.root()}/capabilities/scenario.create.yaml`;
+      await timed(`load: ${file}`, async () => {
+        let [ns, cmReconcile, cmWatch, cmValidate, cmMutate] =
+          await trc.load(file);
+        await fullCreate([ns, cmReconcile, cmWatch, cmValidate, cmMutate]);
+
+        await K8s(kind[cmReconcile.kind]).Delete(cmReconcile);
+        await K8s(kind[cmWatch.kind]).Delete(cmWatch);
+        await K8s(kind[cmValidate.kind]).Delete(cmValidate);
+        await K8s(kind[cmMutate.kind]).Delete(cmMutate);
+
+        logz = await logs();
+      });
+    }, mins(2));
+
+    it(
+      "uses provided alias",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes('"msg":"external api call (validate-create-alias):'),
+        );
+
+        expect(results).toEqual(
+          expect.arrayContaining([
+            expect.stringMatching("alias:create:validate"),
+          ]),
+        );
+      },
+      secs(10),
+    );
+
+    it(
+      "uses default alias",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes(
+            '"msg":"external api call (validate-create-default-alias):',
+          ),
+        );
+
+        expect(results).toEqual(
+          expect.arrayContaining([expect.stringMatching("no alias provided")]),
+        );
+      },
+      secs(10),
+    );
+
+    it(
+      "does not log alias if alias child logger not used",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes(
+            '"msg":"external api call (validate-create-no-child-logger):',
+          ),
+        );
+
+        expect(results).toEqual(
+          expect.not.arrayContaining([expect.stringMatching('"alias":')]),
+        );
+      },
+      secs(10),
+    );
+  });
+
+  describe("create - mutate", () => {
+    let logz: string[];
+    beforeAll(async () => {
+      const file = `${trc.root()}/capabilities/scenario.create.yaml`;
+      await timed(`load: ${file}`, async () => {
+        let [ns, cmReconcile, cmWatch, cmValidate, cmMutate] =
+          await trc.load(file);
+        await fullCreate([ns, cmReconcile, cmWatch, cmValidate, cmMutate]);
+
+        await K8s(kind[cmReconcile.kind]).Delete(cmReconcile);
+        await K8s(kind[cmWatch.kind]).Delete(cmWatch);
+        await K8s(kind[cmValidate.kind]).Delete(cmValidate);
+        await K8s(kind[cmMutate.kind]).Delete(cmMutate);
+
+        logz = await logs();
+      });
+
+    }, mins(2));
+
+    it(
+      "uses provided alias",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes('"msg":"external api call (mutate-create-alias):'),
+        );
+
+        expect(results).toEqual(
+          expect.arrayContaining([
+            expect.stringMatching("alias:create:mutate"),
+          ]),
+        );
+      },
+      secs(10),
+    );
+
+    it(
+      "uses default alias",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes('"msg":"external api call (mutate-create-default-alias):'),
+        );
+
+        expect(results).toEqual(
+          expect.arrayContaining([expect.stringMatching("no alias provided")]),
+        );
+      },
+      secs(10),
+    );
+
+    it(
+      "does not log alias if alias child logger not used",
+      async () => {
+        let results = logz.filter(l =>
+          l.includes(
+            '"msg":"external api call (mutate-create-no-child-logger):',
+          ),
+        );
+
+        expect(results).toEqual(
+          expect.not.arrayContaining([expect.stringMatching('"alias":')]),
         );
       },
       secs(10),
--- hello-pepr-alias/capabilities/alias.ts
+++ hello-pepr-alias/capabilities/alias.ts
@@ -4,10 +4,10 @@ export const HelloPeprAlias = new Capability({
   name: "hello-pepr-alias",
   description: "hello-pepr-alias",
   namespaces: [
-    "hello-pepr-finalize-create",
-    "hello-pepr-finalize-createorupdate",
-    "hello-pepr-finalize-update",
-    "hello-pepr-finalize-delete",
+    "hello-pepr-alias-create",
+    "hello-pepr-alias-createorupdate",
+    "hello-pepr-alias-update",
+    "hello-pepr-alias-delete",
   ],
 });
 
@@ -15,95 +15,187 @@ const { When } = HelloPeprAlias;
 
 When(a.ConfigMap)
   .IsCreated()
-  .InNamespace("hello-pepr-finalize-create")
+  .InNamespace("hello-pepr-alias-create")
   .WithName("cm-reconcile-create")
   .Alias("alias:create:reconcile")
   .Reconcile(function reconcileCreate(cm, phase, logger) {
-    logger.info(cm, "external api call (reconcile-create-alias): reconcile/callback")
+    logger.info(
+      cm,
+      "external api call (reconcile-create-alias): reconcile/callback",
+    );
   })
   .Finalize(function finalizeCreate(cm) {
-    Log.info(cm, "external api call (reconcile-create-alias): reconcile/finalize")
+    Log.info(
+      cm,
+      "external api call (reconcile-create-alias): reconcile/finalize",
+    );
   });
 
-  When(a.ConfigMap)
+When(a.ConfigMap)
   .IsCreated()
-  .InNamespace("hello-pepr-finalize-create")
+  .InNamespace("hello-pepr-alias-create")
   .WithName("cm-reconcile-create")
   .Alias("alias:create:reconcile:finalize")
   .Reconcile(function reconcileCreate(cm) {
-    Log.info(cm, "external api call (reconcile-create-alias): reconcile/callback")
+    Log.info(
+      cm,
+      "external api call (reconcile-create-alias): reconcile/callback",
+    );
   })
   .Finalize(function finalizeCreate(cm, logger) {
-    logger.info(cm, "external api call (reconcile-create-alias): reconcile/finalize")
+    logger.info(
+      cm,
+      "external api call (reconcile-create-alias): reconcile/finalize",
+    );
   });
 
 When(a.ConfigMap)
   .IsCreated()
-  .InNamespace("hello-pepr-finalize-create")
+  .InNamespace("hello-pepr-alias-create")
   .WithName("cm-reconcile-create")
   .Reconcile(function reconcileCreate(cm, phase, logger) {
-    logger.info(cm, "external api call (reconcile-create-default-alias): reconcile/callback")
-  })
+    logger.info(
+      cm,
+      "external api call (reconcile-create-default-alias): reconcile/callback",
+    );
+  });
 
 When(a.ConfigMap)
   .IsCreated()
-  .InNamespace("hello-pepr-finalize-create")
+  .InNamespace("hello-pepr-alias-create")
   .WithName("cm-reconcile-create")
   .Reconcile(function reconcileCreate(cm) {
-    Log.info(cm, "external api call (reconcile-create-no-child-logger): reconcile/callback")
+    Log.info(
+      cm,
+      "external api call (reconcile-create-no-child-logger): reconcile/callback",
+    );
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-watch-create")
+  .Alias("alias:create:watch")
+  .Watch(function watchCreate(cm, phase, logger) {
+    logger.info(
+      cm,
+      "external api call (watch-create-alias): watch/callback",
+    );
+  })
+  .Finalize(function finalizeCreate(cm) {
+    Log.info(
+      cm,
+      "external api call (watch-create-alias): watch/finalize",
+    );
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-watch-create")
+  .Alias("alias:create:watch:finalize")
+  .Watch(function watchCreate(cm) {
+    Log.info(
+      cm,
+      "external api call (watch-create-alias): watch/callback",
+    );
+  })
+  .Finalize(function finalizeCreate(cm, logger) {
+    logger.info(
+      cm,
+      "external api call (watch-create-alias): watch/finalize",
+    );
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-watch-create")
+  .Watch(function watchCreate(cm, phase, logger) {
+    logger.info(
+      cm,
+      "external api call (watch-create-default-alias): watch/callback",
+    );
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-watch-create")
+  .Watch(function watchCreate(cm) {
+    Log.info(
+      cm,
+      "external api call (watch-create-no-child-logger): watch/callback",
+    );
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-validate-create")
+  .Alias("alias:create:validate")
+  .Validate(function validateCreate(cm, logger) {
+    logger.info(
+      cm,
+      "external api call (validate-create-alias): validate/callback",
+    );
+    return cm.Approve();
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-reconcile-create")
+  .Validate(function validateCreate(cm, logger) {
+    logger.info(
+      cm,
+      "external api call (validate-create-default-alias): validate/callback",
+    );
+    return cm.Approve();
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-validate-create")
+  .Validate(function validateCreate(cm) {
+    Log.info(
+      cm,
+      "external api call (validate-create-no-child-logger): validate/callback",
+    );
+    return cm.Approve();
+  });
+
+When(a.ConfigMap)
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-mutate-create")
+  .Alias("alias:create:mutate")
+  .Mutate(function mutateCreate(cm, logger) {
+    logger.info(
+      cm,
+      "external api call (mutate-create-alias): mutate/callback",
+    );
   })
 
 When(a.ConfigMap)
   .IsCreated()
-  .InNamespace("hello-pepr-finalize-create")
-  .WithName("cm-watch-create")
-  .Watch(function watchCreate(cm) {
-    Log.info(cm, "external api call (create): watch/callback")
-  })
-  .Finalize(function finalizeCreate(cm) {
-    Log.info(cm, "external api call (create): watch/finalize")
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-mutate-create")
+  .Mutate(function mutateCreate(cm, logger) {
+    logger.info(
+      cm,
+      "external api call (mutate-create-default-alias): mutate/callback",
+    );
   });
 
 When(a.ConfigMap)
-  .IsCreatedOrUpdated()
-  .InNamespace("hello-pepr-finalize-createorupdate")
-  .WithName("cm-watch-createorupdate")
-  .Watch(function watchCreateOrUpdate(cm) {
-    // delete with finalizer causes an UPDATE with deletionTimestamp; ignore it
-    if (cm.metadata?.deletionTimestamp) { return }
-
-    Log.info(cm, "external api call (createorupdate): watch/callback")
-  })
-  .Finalize(function finalizeCreateOrUpdate(cm) {
-    Log.info(cm, "external api call (createorupdate): watch/finalize")
-  });
-
-When(a.ConfigMap)
-  .IsUpdated()
-  .InNamespace("hello-pepr-finalize-update")
-  .WithName("cm-watch-update")
-  .Watch(function watchUpdate(cm) {
-    // delete with finalizer triggers an UPDATE to add deletionTimestamp; ignore it
-    if (cm.metadata?.deletionTimestamp) { return }
-
-    Log.info(cm, "external api call (update): watch/callback")
-  })
-  .Finalize(function finalizeUpdate(cm) {
-    Log.info(cm, "external api call (update): watch/finalize")
-  });
-
-  When(a.ConfigMap)
-  .IsDeleted()
-  .InNamespace("hello-pepr-finalize-delete")
-  .WithName("cm-watch-delete")
-  .Watch(function watchDelete(cm) {
-    // Here be dragons!
-    //
-    // Due to the way kubernetes finalizers work, this deletion callback will fire
-    //  AFTER the finalizer callback fires -- be sure to expect & code for this!
-    //  (see: https://kubernetes.io/docs/concepts/overview/working-with-objects/finalizers/#how-finalizers-work )
-    Log.info(cm, "external api call (delete): watch/callback")
-  })
-  .Finalize(function finalizeDelete(cm) {
-    Log.info(cm, "external api call (delete): watch/finalize")
+  .IsCreated()
+  .InNamespace("hello-pepr-alias-create")
+  .WithName("cm-mutate-create")
+  .Mutate(function mutateCreate(cm) {
+    Log.info(
+      cm,
+      "external api call (mutate-create-no-child-logger): mutate/callback",
+    );
   });

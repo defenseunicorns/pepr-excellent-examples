682a19d694823b4678d3a4aa147dba1507d20df9
Sam Mayer <sam.mayer@defenseunicorns.com>
2024-09-26 14:42:46 -0500

    Remove unsupported test cases

--- test-specific-version/capabilities/versions.e2e.test.ts
+++ test-specific-version/capabilities/versions.e2e.test.ts
@@ -1,22 +1,16 @@
 import { describe, it, expect, beforeAll, afterAll } from "@jest/globals";
 import { execSync } from "child_process";
-import findUp from 'find-up' // TODO: Would rather use findUpSync()
 import {getPeprAlias} from '../../_helpers/src/pepr'
 
 describe('version tests', () => {
   describe('when pepr version is defined in an example folder (v0.31.1)', () => { 
-    const peprAlias = 'pepr@0.31.1'
     beforeAll(() =>{
       execSync(`rm -rf node_modules/pepr`)
     })
 
     it('shows the correct version', ()=>{
-      const result = execSync(`npx ${peprAlias} --version`).toString()
+      const result = execSync(`npx pepr --version`).toString()
       expect(result).toContain('0.31.1');
-    })
-    it('shows the help menu without --unpublished', () =>{
-      const result = execSync(`npx ${peprAlias} init --help`).toString()
-      expect(result).not.toContain('--unpublished')
     })
   })
   describe('when pepr version matches the top-level package.json file (v0.36.0)', () => { 
@@ -26,25 +20,14 @@ describe('version tests', () => {
     })
 
     it('shows the correct version', ()=>{
-      const result = execSync('npx pepr --version').toString()
+      const result = execSync(`npx pepr --version`).toString()
       expect(result).toContain('0.36.0');
     })
-    it('shows the help menu without --unpublished', () =>{
-      const result = execSync('npx pepr init --help').toString()
-      expect(result).not.toContain('--unpublished')
-    })
   })
-  describe('when pepr is a local dev copy', () => { 
+  describe('when pepr is a development copy (--local-package or --custom-package)', () => { 
     const peprBuildName = 'pepr-0.0.0-development.tgz';
-    let peprBuildPath = '';
-    let peprAlias = process.env.PEPR_PACKAGE ? process.env.PEPR_PACKAGE : 'pepr';
-    peprAlias = process.env.PEPR_PACKAGE ? process.env.PEPR_PACKAGE : process.env.PEPR_PACKAGE as string;
-    let peprExcellentExamplesRepo = '';
     beforeAll(async () =>{
-      peprBuildPath = await findUp(peprBuildName) as string //TODO: Type coercion
-      peprAlias = `file:${peprBuildPath}`
-      peprExcellentExamplesRepo = await findUp('pepr-excellent-examples', {type: 'directory'}) as string //TODO: type coercion
-      expect(execSync(`ls -l ${process.env.PEPR_PACKAGE}`, {cwd: peprExcellentExamplesRepo}).toString()).toContain(peprBuildName)
+      expect(execSync(`ls -l ${process.env.PEPR_PACKAGE}`).toString()).toContain(peprBuildName)
     })
 
     afterAll(()=>{
@@ -53,12 +36,8 @@ describe('version tests', () => {
     })
 
     it('shows the correct version', ()=>{
-      const result = execSync(`npx --yes ${getPeprAlias()} --version`, {cwd: peprExcellentExamplesRepo}).toString()
+      const result = execSync(`npx --yes ${getPeprAlias()} --version`).toString()
       expect(result).toContain('0.0.0-development');
-    })
-    it('shows the help menu with --unpublished', () =>{
-      const result = execSync(`npx --yes ${getPeprAlias()} init --help`, {cwd: peprExcellentExamplesRepo}).toString()
-      expect(result).toContain('--unpublished')
     })
   })
 })

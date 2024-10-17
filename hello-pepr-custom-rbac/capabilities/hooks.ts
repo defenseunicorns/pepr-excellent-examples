81d66b626362a808f7384870dbcb7cf16f5d836e
Kim Schaefer <kim@defenseunicorns.com>
2024-10-17 10:50:57 -0600

    initial test template created

---
+++ hello-pepr-custom-rbac/capabilities/hooks.ts
@@ -0,0 +1,38 @@
+import { Capability, a } from "pepr";
+
+const name = "hello-pepr-hooks";
+
+export const HelloPeprHooks = new Capability({
+  name: name,
+  description: name,
+  namespaces: [name],
+});
+const { When } = HelloPeprHooks;
+
+When(a.Secret)
+  .IsCreated()
+  .InNamespace(name)
+  .WithName("mutate-only")
+  .Mutate(function mutateOnly() {});
+
+When(a.Secret)
+  .IsCreated()
+  .InNamespace(name)
+  .WithName("validate-only")
+  .Validate(function validateOnly(request) {
+    return request.Approve();
+  });
+
+When(a.Secret)
+  .IsCreated()
+  .InNamespace(name)
+  .WithName("mutate-and-validate")
+  .Mutate(function mutateMutVal() {});
+
+When(a.Secret)
+  .IsCreated()
+  .InNamespace(name)
+  .WithName("mutate-and-validate")
+  .Validate(function validateMutVal(request) {
+    return request.Approve();
+  });

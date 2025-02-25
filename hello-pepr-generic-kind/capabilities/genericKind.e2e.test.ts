c593656cd98d4ecbb0aaa8f0afdecd6ebe0e8bef
Case Wylie <cmwylie19@defenseunicorns.com>
2025-02-25 15:39:00 -0500

    chore: generic-kind e2e
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

---
+++ hello-pepr-generic-kind/capabilities/genericKind.e2e.test.ts
@@ -0,0 +1,104 @@
+import { beforeAll, afterAll, describe, it, expect } from "@jest/globals";
+import { TestRunCfg } from "helpers/src/TestRunCfg";
+import { mins, secs } from "helpers/src/time";
+import { fullCreate } from "helpers/src/general";
+import { K8s, kind } from "kubernetes-fluent-client";
+import { clean } from "helpers/src/cluster";
+import { moduleUp, moduleDown, untilLogged } from "helpers/src/pepr";
+import { execSync } from "child_process";
+
+const trc = new TestRunCfg(__filename);
+const name = "hello-pepr-generic-kind";
+describe("genericKind.ts", () => {
+  beforeAll(async () => {
+    /*
+     * load the CRD into Kubernetes BEFORE the module starts up
+     * so the watches can be created
+     */
+    const file = `${trc.root()}/capabilities/scenario.setup.yaml`;
+    const resources = await trc.load(file);
+    await fullCreate(resources, kind);
+    await moduleUp()
+  }, mins(2));
+
+  afterAll(async () => {
+    await moduleDown();
+    await clean(trc);
+  }, mins(2));
+
+
+  it("watches resource creates", async () => {
+    const file = `${trc.root()}/capabilities/scenario.create.yaml`;
+    execSync(`kubectl apply -f ${file}`);
+
+    await untilLogged("GenericKind created and mutated");
+    await untilLogged("GenericKind created and validated");
+    await untilLogged("GenericKind created and watched");
+  }, secs(10));
+
+  it("watches resource create-or-updates", async () => {
+    const file = `${trc.root()}/capabilities/scenario.create-or-update.yaml`;
+    const resources = await trc.load(file);
+    execSync(`kubectl apply -f ${file}`);
+
+    // no direct assertion -- test succeeds when message is logged
+    await untilLogged("GenericKind created or updated and mutated");
+    await untilLogged("GenericKind created or updated and validated");
+    await untilLogged("GenericKind created or updated and watched");
+  }, secs(10));
+
+  it("watches resource updates", async () => {
+    const file = `${trc.root()}/capabilities/scenario.update.yaml`;
+    execSync(`kubectl apply -f ${file}`);
+
+    await K8s(kind.GenericKind,{
+      kindOverride: {
+        group: "pepr.dev",
+        version: "v1",
+        kind: "Unicorn",
+      }
+    }).Apply({
+      metadata: {
+        name: "update",
+        namespace: name,
+      },
+      spec: {
+        message: "updated",
+        counter: Math.random(),
+      },
+    }, { force: true});
+  
+
+    // no direct assertion -- test succeeds when message is logged
+    await untilLogged("GenericKind updated and mutated");
+    await untilLogged("GenericKind updated and validated");
+    await untilLogged("GenericKind updated and watched");
+  }, secs(10));
+
+  it("watches resource deletes", async () => {
+    const file = `${trc.root()}/capabilities/scenario.delete.yaml`;
+    execSync(`kubectl apply -f ${file}`);
+
+    await K8s(kind.GenericKind,{
+      kindOverride: {
+        group: "pepr.dev",
+        version: "v1",
+        kind: "Unicorn",
+      }
+    }).Delete({
+      metadata: {
+        name: "delete",
+        namespace: name,
+      },
+      spec: {
+        message: "updated",
+        counter: Math.random(),
+      },
+    });
+
+    // no direct assertion -- test succeeds when message is logged
+    await untilLogged("GenericKind deleted mutated");
+    await untilLogged("GenericKind deleted validated");
+    await untilLogged("GenericKind deleted watched");
+  }, secs(10));
+});
---
+++ hello-pepr-generic-kind/capabilities/genericKind.ts
@@ -0,0 +1,84 @@
+import { Capability, Log, a } from "pepr";
+
+const name = "hello-pepr-generic-kind";
+
+export const HelloPeprGenericKind = new Capability({
+  name: name,
+  description: "Assert that generic kinds work through admission and watch",
+  namespaces: [name],
+});
+
+const { When } = HelloPeprGenericKind;
+When(a.CustomResourceDefinition)
+When(a.GenericKind, {
+  group: "pepr.dev",
+  version: "v1",
+  kind: "Unicorn",
+})
+  .IsCreated()
+  .WithName("create")
+  .Mutate(() => {
+    Log.info("GenericKind created and mutated");
+  })
+  .Validate((instance) => {
+    Log.info("GenericKind created and validated");
+    return instance.Approve();
+  })
+  .Watch(() => {
+    Log.info("GenericKind created and watched");
+  });
+
+When(a.GenericKind, {
+  group: "pepr.dev",
+  version: "v1",
+  kind: "Unicorn",
+})
+  .IsCreatedOrUpdated()
+  .WithName("create-or-update")
+  .Mutate(() => {
+    Log.info("GenericKind created or updated and mutated");
+  })
+  .Validate((instance) => {
+    Log.info("GenericKind created or updated and validated");
+    return instance.Approve();
+  })
+  .Watch(() => {
+    Log.info("GenericKind created or updated and watched");
+  });
+
+When(a.GenericKind, {
+  group: "pepr.dev",
+  version: "v1",
+  kind: "Unicorn",
+})
+  .IsUpdated()
+  .WithName("update")
+  .Mutate(() => {
+    Log.info("GenericKind updated and mutated");
+  })
+  .Validate((instance) => {
+    Log.info("GenericKind updated and validated");
+    return instance.Approve();
+  })
+  .Watch(() => {
+    Log.info("GenericKind updated and watched");
+  });
+
+When(a.GenericKind, {
+  group: "pepr.dev",
+  version: "v1",
+  kind: "Unicorn",
+})
+  .IsDeleted()
+  .WithName("delete")
+  .Mutate(() => {
+    Log.info("GenericKind deleted mutated");
+  })
+  .Validate((instance) => {
+    Log.info("GenericKind deleted validated");
+    return instance.Approve();
+  })
+  .Watch(() => {
+    Log.info("GenericKind deleted watched");
+  });
+

b0ffc7792803e4f7a503e6d2c65df49cf28b0b5f
Sam Mayer <sam.mayer@defenseunicorns.com>
2025-03-26 16:35:10 -0500

    Fix module declaration

--- hello-pepr-warnings/pepr.ts
+++ hello-pepr-warnings/pepr.ts
@@ -1,11 +1,11 @@
 import { PeprModule } from "pepr";
+import cfg from "./package.json"
 import { HelloPeprWarnings } from "./capabilities/warnings";
 
 /**
  * This module demonstrates the use of warnings in validation responses
  */
-new PeprModule({
-  name: "hello-pepr-warnings",
-  description: "Testing warnings in validation responses",
-  capabilities: [HelloPeprWarnings],
-});
+new PeprModule(
+  cfg,
+  [HelloPeprWarnings],
+);

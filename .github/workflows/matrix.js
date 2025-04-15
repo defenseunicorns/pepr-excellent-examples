cec8133eb86f802cb9fdcd58fcf7c37e0fd1e9a5
Case Wylie <cmwylie19@defenseunicorns.com>
2025-04-15 13:14:41 -0400

    chore: skip version test
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- .github/workflows/matrix.js
+++ .github/workflows/matrix.js
@@ -15,15 +15,20 @@ let examples = stdout.toLocaleString().trim().split("\n");
 
 // select those with 'test:e2e' scripts
 let e2es = examples
-  .map(ex => [ex, require(`${ex}/package.json`)])
+  .map((ex) => [ex, require(`${ex}/package.json`)])
   .filter(([ex, cfg]) => Object.hasOwn(cfg.scripts, "test:e2e"));
 
 // gen matrix spec
 let spec = {
-  include: e2es.map(([ex, cfg]) => ({
-    name: cfg.name,
-    path: ex,
-  })),
+  include: e2es.map(([ex, cfg]) => {
+    // requires package.json.bak which is only present when overriding the version
+    if (cfg.name !== "test-specific-version") {
+      return {
+        name: cfg.name,
+        path: ex,
+      };
+    }
+  }),
 };
 
 console.log(JSON.stringify(spec));

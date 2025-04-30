bbdedf1770e6624142718295b6ca0810e3836857
Case Wylie <cmwylie19@defenseunicorns.com>
2025-04-30 15:41:52 -0400

    chore: assert on store cr (#281)
    This PR tests the store migration of a previously existing store and
    asserts based on the store CR itself
    
    ---------
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>
    Co-authored-by: Sam Mayer <sam.mayer@defenseunicorns.com>

---
+++ hello-pepr-store-resources/capabilities/store.e2e.test.ts
@@ -0,0 +1,70 @@
+import { beforeAll, afterAll, describe, it, expect } from "@jest/globals";
+import { kind, K8s } from "kubernetes-fluent-client";
+import { TestRunCfg } from "helpers/src/TestRunCfg";
+import { fullCreate } from "helpers/src/general";
+import { secs, mins, timed, sleep } from "helpers/src/time";
+import { moduleUp, moduleDown, untilLogged, logs } from "helpers/src/pepr";
+import { clean } from "helpers/src/cluster";
+import { execSync } from "child_process";
+import { PeprStore } from "./peprstore-v1";
+const apply = async res => {
+  return await fullCreate(res, kind);
+};
+
+const trc = new TestRunCfg(__filename);
+
+describe("store.ts", () => {
+  beforeAll(async () => {
+    execSync(`kubectl apply -f ${trc.root()}/capabilities/peprstore.crd.yaml`, {
+      stdio: "inherit",
+    });
+    await K8s(kind.Namespace).Apply({
+      apiVersion: "v1",
+      kind: "Namespace",
+      metadata: {
+        name: "pepr-system",
+      },
+    });
+    execSync(
+      `kubectl apply -f ${trc.root()}/capabilities/nonmigrated-peprstore.yaml`,
+      { stdio: "inherit" },
+    );
+    await moduleUp(), mins(2);
+  }, secs(90));
+
+  describe("Store Resource", () => {
+    let store: PeprStore;
+    let logz;
+    beforeAll(async () => {
+      const file = `${trc.root()}/capabilities/scenario.observe.yaml`;
+      await timed(`load: ${file}`, async () => {
+        const resources = await trc.load(file);
+        await apply(resources);
+
+        await untilLogged('"msg":"observed"');
+        logz = await logs();
+      });
+    }, mins(1));
+    it("should migrate the store resource to v2", async () => {
+      execSync("kubectl get peprstore -n pepr-system", {
+        stdio: "inherit",
+      });
+      store = await K8s(PeprStore)
+        .InNamespace("pepr-system")
+        .Get("pepr-store-resources-store");
+      expect(store).toBeDefined();
+      console.log(JSON.stringify(store, null, 2));
+      expect(store!.data!["hello-pepr-store-resources-v2-testing"]).toBe("migrated");
+    });
+    it("write the correct data to the store", () => {
+      expect(true).toBe(true);
+      console.log(JSON.stringify(store.data, null, 2));
+      expect(store!.data!["hello-pepr-store-resources-v2-testing"]).toBe("migrated");
+      expect(store!.data!["hello-pepr-store-resources-v2-a"]).toBe("1");
+      expect(store!.data!["hello-pepr-store-resources-v2-b"]).toBe("2");
+      expect(store!.data!["hello-pepr-store-resources-v2-c"]).toBe("3");
+      expect(store!.data!["hello-pepr-store-resources-v2-https://observed"]).toBe("yay");
+      expect(store!.data!["__pepr_do_not_delete__"]).toBe("k-thx-bye");
+    });
+  });
+});

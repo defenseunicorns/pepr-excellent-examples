5cef1488cc6b63adaa13a25debb3fb940ea5492e
Case Wylie <cmwylie19@defenseunicorns.com>
2025-09-02 13:29:20 -0400

    chore: test pr in yaml and ts
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- hello-pepr-ns-all/capabilities/namespace.ts
+++ hello-pepr-ns-all/capabilities/namespace.ts
@@ -17,14 +17,9 @@ When(a.ConfigMap)
   });
 
 /**
- * This policy prevents pods from sharing the host namespaces.
+ * This policy mutates Pods by setting a ns=seen annotation when they are created in any namespace.
  *
- * Host namespaces (Process ID namespace, Inter-Process Communication namespace, and network namespace)
- * allow access to shared information and can be used to elevate privileges. Pods should not be allowed
- * access to host namespaces. This policy ensures fields which make use of these host namespaces are
- * set to `false`.
- *
- * @related https://repo1.dso.mil/big-bang/product/packages/kyverno-policies/-/blob/main/chart/templates/disallow-host-namespaces.yaml
+ * @related https://docs.pepr.dev
  *
  * @lulaStart 123e4567-e89b-12d3-a456-426614174000
  */

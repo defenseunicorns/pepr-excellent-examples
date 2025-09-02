3df71153ed317c5dd8e1416b8d3b2e184d5f5258
Case Wylie <cmwylie19@defenseunicorns.com>
2025-09-02 13:26:39 -0400

    chore: test pr in yaml and ts
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- hello-pepr-ns-all/capabilities/namespace.ts
+++ hello-pepr-ns-all/capabilities/namespace.ts
@@ -15,15 +15,28 @@ When(a.ConfigMap)
   .Mutate(function mutateDef(request) {
     request.SetAnnotation("def", "seen");
   });
-// @lulaStart 123e4567-e89b-12d3-a456-426614174000
+
+/**
+ * This policy prevents pods from sharing the host namespaces.
+ *
+ * Host namespaces (Process ID namespace, Inter-Process Communication namespace, and network namespace)
+ * allow access to shared information and can be used to elevate privileges. Pods should not be allowed
+ * access to host namespaces. This policy ensures fields which make use of these host namespaces are
+ * set to `false`.
+ *
+ * @related https://repo1.dso.mil/big-bang/product/packages/kyverno-policies/-/blob/main/chart/templates/disallow-host-namespaces.yaml
+ *
+ * @lulaStart 123e4567-e89b-12d3-a456-426614174000
+ */
 When(a.ConfigMap)
   .IsCreated()
   .InNamespace(name)
   .Mutate(function mutateNs(request) {
-    // Set the annotation.
     request.SetAnnotation("ns", "seen");
   });
-// @lulaEnd 123e4567-e89b-12d3-a456-426614174000
+/**
+ * @lulaEnd 123e4567-e89b-12d3-a456-426614174000
+ */
 When(a.ConfigMap)
   .IsCreated()
   .Mutate(function mutateNon(request) {

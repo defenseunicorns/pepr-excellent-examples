0b65d8c7d79055e039416b94d0b9a22d72297bd0
Case Wylie <cmwylie19@defenseunicorns.com>
2025-10-06 11:16:51 -0400

    chore: testing
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- hello-pepr-ns-all/capabilities/namespace.ts
+++ hello-pepr-ns-all/capabilities/namespace.ts
@@ -27,7 +27,7 @@ When(a.ConfigMap)
   .IsCreated()
   .InNamespace(name)
   .Mutate(function mutateNs(request) {
-    // leave comment - with dismiss
+    // leave comment
     request.SetAnnotation("ns", "seen");
   });
 /**

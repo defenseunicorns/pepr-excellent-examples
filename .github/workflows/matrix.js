721142dc58bf0cf442482659fc03b90310d5ddc1
Case Wylie <cmwylie19@defenseunicorns.com>
2026-02-23 10:31:35 -0500

    chore: remove test-specific-version (#464)
    This is the only remaining test that is failing due to ESLint dependency
    conflicts. The test basically runs `npx pepr --version` which specific
    versions (development and non). Which cause conflicts and fail.
    
    We have this logic in integration tests. The question is, is this test
    worth spending engineering cycles fixing or is it redundant at this
    point
    
    
    https://github.com/defenseunicorns/pepr/actions/runs/22241401172/job/64345967213?pr=2951#step:11:169
    
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- .github/workflows/matrix.js
+++ .github/workflows/matrix.js
@@ -16,8 +16,7 @@ let examples = stdout.toLocaleString().trim().split("\n");
 // select those with 'test:e2e' scripts
 let e2es = examples
   .map(ex => [ex, require(`${ex}/package.json`)])
-  .filter(([ex, cfg]) => Object.hasOwn(cfg.scripts, "test:e2e"))
-  .filter(([ex, cfg]) => cfg.name !== "test-specific-version") // requires package.json.bak which is only present when overriding the Pepr version
+  .filter(([ex, cfg]) => Object.hasOwn(cfg.scripts, "test:e2e"));
 
 // gen matrix spec
 let spec = {
@@ -26,5 +25,5 @@ let spec = {
     path: ex,
   })),
 };
-  
+
 console.log(JSON.stringify(spec));
